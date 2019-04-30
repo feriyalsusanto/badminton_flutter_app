39dc164b8805a42e085fdb88d81ebe38b4467265
feriyalsusanto <feriyalsusanto@gmail.com>
2019-04-30 14:02:17 +0700

    add video player dependencies

---
+++ lib/home/menu/videoplayer.dart
@@ -0,0 +1,51 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayer extends StatefulWidget {
+  VideoPlayer(this.title, this.url);
+
+  String title;
+  String url;
+
+  @override
+  _VideoPlayerState createState() {
+    return _VideoPlayerState();
+  }
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  ChewieController _chewieController;
+  VideoPlayerController _videoPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController = VideoPlayerController.network(widget.url);
+    _chewieController = ChewieController(
+        videoPlayerController: _videoPlayerController,
+        aspectRatio: 3 / 2,
+        autoInitialize: true,
+        looping: true);
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Chewie(
+        controller: _chewieController,
+      ),
+    );
+  }
+}
